605b86adfd43ee523f3f4e068508814957bea5cd
ProjectOne <thanhnguyensonjqk@gmail.com>
2025-12-22 01:04:40 +0700

    Add PrivateRouter and protect authenticated routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Filter from "./pages/Filter";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import { Toaster } from "react-hot-toast";
+import PrivateRouter from "./components/PrivateRouter";
 
 const App = () => {
     return (
@@ -14,12 +15,47 @@ const App = () => {
             <Toaster />
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Navigate to="/login" />} />
-                    <Route path="/dashboard" element={<Home />} />
-                    <Route path="/income" element={<Income />} />
-                    <Route path="/expense" element={<Expense />} />
-                    <Route path="/category" element={<Category />} />
-                    <Route path="/filter" element={<Filter />} />
+                    <Route path="/" element={<Navigate to="/login" replace />} />
+                    <Route 
+                        path="/dashboard" 
+                        element={
+                            <PrivateRouter>
+                                <Home/>
+                            </PrivateRouter>
+                        } 
+                    />
+                    <Route 
+                        path="/income" 
+                        element={
+                            <PrivateRouter>
+                                <Income />
+                            </PrivateRouter>
+                        } 
+                    />
+                    <Route 
+                        path="/expense" 
+                        element={
+                            <PrivateRouter>
+                                <Expense />
+                            </PrivateRouter>
+                        }
+                    />
+                    <Route
+                        path="/category"
+                        element={
+                            <PrivateRouter>
+                                <Category />
+                            </PrivateRouter>
+                        }
+                    />
+                    <Route
+                        path="/filter"
+                        element={
+                            <PrivateRouter>
+                                <Filter />
+                            </PrivateRouter>
+                        }
+                    />
                     <Route path="/login" element={<Login />} />
                     <Route path="/signup" element={<Signup />} />
                 </Routes>
@@ -27,5 +63,4 @@ const App = () => {
         </>
     )
 }
-
 export default App;
--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -6,9 +6,13 @@ export const AppContext = createContext();
 export const AppContextProvider = ({children}) => {
     const [user,setUser] = useState(null);
     
+    const clearUser = () => {
+        setUser(null);
+    }
     const contextValue = {
         user,
-        setUser
+        setUser,
+        clearUser
     }
 
     return (
